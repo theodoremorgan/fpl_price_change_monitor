d0085b19ee39b8231e2b236c5b7ec894be656043
theo <theodore.joseph.morgan@gmail.com>
2024-11-01 16:39:40 +0000

    get_player_id done

--- watchlist_handler/config.py
+++ watchlist_handler/config.py
@@ -3,3 +3,6 @@ date_format = '%Y_%m_%d'
 data_dir_path = './data/'
 watchlist_dir = 'watchlist/'
 price_change_dir = 'price_change/'
+
+#fantasy premier league urls
+fantasy_base_url = 'https://fantasy.premierleague.com/api/'
